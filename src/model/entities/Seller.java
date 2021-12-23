29f8f58743dff57cc288a6394f5d91e522a48bbf
adcorrea <antoniopucc@hotmail.com>
2021-12-23 17:10:07 -0300

    Seller class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,9 +1,15 @@
 package model.entities;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable {
 	
+
+	private static final long serialVersionUID = 1L;
+
+
 	private Integer id;
 	private String name;
 	private String email;
@@ -84,5 +90,31 @@ public class Seller {
 	
 	
 	
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
 	
 }
